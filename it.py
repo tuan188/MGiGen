fe23fbbe776700fbbf0a862aeb0a1486ca8b77ab
Tuan Truong <tuan188@gmail.com>
2018-06-29 16:27:41 +0700

    Add mock tool

--- it.py
+++ it.py
@@ -1413,6 +1413,81 @@ class JSON(object):
 
 #=================== Commands ===================
 
+
+class Mock(object):
+
+	DEFAULT_VALUES = {
+		"Int": "0",
+		"Bool": "false",
+		"String": '""',
+		"Double": "0.0",
+		"Float": "0.0",
+		"Date": "Date()"
+	}
+
+	SYSTEM_TYPES = { "Int", "Bool", "String", "Double", "Float", "Date" }
+
+	class Function(object):
+		def __init__(self, origin, name, return_type):
+			super(Mock.Function, self).__init__()
+			self.origin = origin
+			self.name = name
+			self.return_type = return_type
+
+		def __str__(self):
+			return self.origin
+
+	def __init__(self, protocol_text):
+		super(Mock, self).__init__()
+		self.protocol_text = protocol_text
+
+	def _get_protocol_name(self, str):
+		regex = re.compile("protocol (\w+)")
+		mo = regex.search(str)
+		protocol_name = mo.group(1)
+		if protocol_name.endswith("Type"):
+			class_name = protocol_name[:-4]
+		elif protocol_name.endswith("Protocol"):
+			class_name = protocol_name[:-8]
+		else:
+			class_name = protocol_name
+		return (protocol_name, class_name)
+
+	def create_mock(self):
+		str = self.protocol_text
+		(protocol_name, class_name) = self._get_protocol_name(str)
+		func_regex = re.compile("func (\w+)\(.*\)( -> (.*))?")
+		funcs = [Mock.Function(f.group(), f.group(1), f.group(3)) for f in func_regex.finditer(str)]
+		content = "final class {}Mock: {} {{\n\n".format(class_name, protocol_name)
+		for f in funcs:
+			content += "    // MARK: - {}\n".format(f.name)
+			content += "    var {}_Called = false\n".format(f.name)
+			if f.return_type == None:
+				return_value = "()"
+			elif f.return_type.endswith("?"):
+				return_value = "nil"
+			elif f.return_type.startswith("Driver"):
+				return_value = "Driver.empty()"
+			elif f.return_type.startswith("Observable"):
+				return_value = "Observable.empty()"
+			elif f.return_type in Mock.SYSTEM_TYPES:
+				return_value = Mock.DEFAULT_VALUES[f.return_type]
+			else:
+				return_value = "{}()".format(f.return_type)
+			if f.return_type != None:
+				content += "    var {}_ReturnValue: {} = {}\n".format(f.name, f.return_type, return_value)
+			content += "\n"
+			content += "    {} {{\n".format(f.origin)
+			content += "        {}_Called = true\n".format(f.name)
+			if f.return_type != None:
+				content += "        return {}_ReturnValue\n".format(f.name)
+			content += "    }\n\n"
+		content += "}\n"
+		return content
+
+
+#=================== Commands ===================
+
 class Commmands:
 	HELP = "help"
 	HEADER = "header"
@@ -1420,7 +1495,7 @@ class Commmands:
 	JSON = "json"
 	MOCK = "mock"
 	API = "api"
-	UT = "ut"
+	UNITTEST = "test"
 
 
 class HelpCommand(object):
@@ -1509,6 +1584,20 @@ class JSONCommand(object):
 		JSON(self.model_name, self.json_text).create_models()
 
 
+class MockCommand(object):
+	def __init__(self):
+		super(MockCommand, self).__init__()
+
+	def create_mock(self):
+		try:
+			protocol_text = pasteboard_read()
+			output = Mock(protocol_text).create_mock()
+			pasteboard_write(output)
+			print("Text has been copied to clipboard.")
+		except:
+			print("Invalid protocol text in clipboard.")
+
+
 #=================== Main ===================
 
 
@@ -1535,6 +1624,8 @@ def execute(args):
 			JSONCommand(model_name, json).create_models()
 		else:
 			print("Missing model name.")
+	elif command == Commmands.MOCK:
+		MockCommand().create_mock()
 	else:
 		print("'{}' is not a valid command. See 'python it.py help'.".format(command))
 
