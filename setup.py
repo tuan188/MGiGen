dad2ecc4e05cc081d8072acff80943f27b4c032f
Tuan Truong <tuan188@gmail.com>
2018-10-09 09:31:35 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
 		]
 	},
 	classifiers=[
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
