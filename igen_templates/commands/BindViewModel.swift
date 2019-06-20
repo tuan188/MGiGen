3e53a43dbb55db38452c1ab5d8c2e9721cdaab0b
Tuan Truong <tuan188@gmail.com>
2019-06-20 16:13:24 +0700

    Update bind command template file

--- igen_templates/commands/BindViewModel.swift
+++ igen_templates/commands/BindViewModel.swift
@@ -1,15 +1,15 @@
     func bindViewModel() {
         let input = {{ name }}ViewModel.Input(
         {% for p in input_properties %}
-            {{ p.name }}: Driver.empty(){{ "," if not loop.last }}
+            {{ p.name }}: Driver.empty(){{ ',' if not loop.last }}
         {% endfor %}
         )
 
         let output = viewModel.transform(input)
-        
+
     {% for p in output_properties %}
         output.{{ p.name }}
               .drive()
-              .disposed(by: rx.disposeBag)
+              .disposed(by: rx.disposeBag){{ '\n' if not loop.last }}
     {% endfor %}
     }
